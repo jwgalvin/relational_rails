026f3622f6e7da4cf7f7b0ce75f78c79d89c8801
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-03 16:42:05 -0700

    feat: tests for user story 2

--- spec/features/Cults/show_spec.rb
+++ spec/features/Cults/show_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 describe "shows a Cult per page" do
+  #This test will need a new route and Clickable cult name
   it "Shows one Cult" do
     cult = Cult.create!(name: "Heaven's Gate", member_size: 323, open_enrollment: true)
     cult_2 = Cult.create!(name: "Branch Davidian", member_size: 42, open_enrollment: false)
     member = cult.members.create!(name: "Marshall Applewhite", married: true, children: 0)
     member_2 = cult.members.create!(name: "Bonnie Nettles", married: true, children: 0)
-    # binding.pry
+    #binding.pry
     visit "/cults/#{cult.id}"
+    save_and_open_page
     expect(page).to have_content(cult.name)
+    expect(page).to have_content(cult.member_size)
+    expect(page).to have_content(cult.open_enrollment)
+    expect(page).to have_content(cult.created_at)
+    expect(page).to have_content(cult.updated_at)
     expect(page).to_not have_content(cult_2.name)
   end
 end
