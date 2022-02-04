2b30c7bd166d6fd43d1ac4adabed830046ffa11d
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-03 17:51:15 -0700

    Test: US4 correct tests in correct spot

--- spec/features/members/show_spec.rb
+++ spec/features/members/show_spec.rb
@@ -1,1 +1,19 @@
+require 'rails_helper'
 
+describe "shows a single page" do
+  it "shows one member name" do
+    cult = Cult.create!(name: "Heaven's Gate", member_size: 323, open_enrollment: true)
+    member = cult.members.create!(name: "Marshall Applewhite", married: true, children: 0)
+    member_2 =cult.members.create!(name: "Bonnie Nettles", married: true, children: 0)
+    visit "/members/#{member.id}"
+    save_and_open_page
+    expect(page).to have_content(member.name)
+    expect(page).to have_content(member.married)
+    expect(page).to have_content(member.created_at)
+    expect(page).to have_content(member.updated_at)
+    expect(page).to have_content(member.children)
+    # expect(page).to have_content(member.id)
+    expect(page).to_not have_content(member_2.name)
+  end
+
+end
