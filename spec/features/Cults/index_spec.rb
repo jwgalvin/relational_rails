da9b7afb6e98141a944e3a74cfd74d2b7c888b88
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-03 19:48:23 -0700

    Test: added gem orderly, test for US 6.

--- spec/features/Cults/index_spec.rb
+++ spec/features/Cults/index_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-describe "index page for members" do
-  xit "can see a page with all members" do
-    cult = Cult.create!(name: "Heaven's Gate", member_size: 323, open_enrollment: true)
-    cult_2 = Cult.create!(name: "Branch Davidian", member_size: 42, open_enrollment: false)
+describe "index page for Cults" do
+  it "can see a page with all members" do
+    heaven = Cult.create!(name: "Heaven's Gate", member_size: 323, open_enrollment: true)
+    waco = Cult.create!(name: "Branch Davidian", member_size: 42, open_enrollment: false)
+    science = Cult.create!(name: "Scientology", member_size: 4000)
     member = cult.members.create!(name: "Marshall Applewhite", married: true, children: 0)
     member_2 =cult.members.create!(name: "Bonnie Nettles", married: true, children: 0)
     visit '/cults'
+
     save_and_open_page
-    expect(page).to have_content(var1.attribute)
+    expect(heaven).to appear_before(waco)
+    expect(waco).to appear_before(science)
   end
 end
