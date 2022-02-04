ce43286f8080163adb5904029ce051ab30676e81
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-03 19:58:35 -0700

    test: fixed US6 test for correct elements

--- spec/features/Cults/index_spec.rb
+++ spec/features/Cults/index_spec.rb
@@ -5,12 +5,12 @@ describe "index page for Cults" do
     heaven = Cult.create!(name: "Heaven's Gate", member_size: 323, open_enrollment: true)
     waco = Cult.create!(name: "Branch Davidian", member_size: 42, open_enrollment: false)
     science = Cult.create!(name: "Scientology", member_size: 4000)
-    member = cult.members.create!(name: "Marshall Applewhite", married: true, children: 0)
-    member_2 =cult.members.create!(name: "Bonnie Nettles", married: true, children: 0)
+    member = heaven.members.create!(name: "Marshall Applewhite", married: true, children: 0)
+    member_2 = heaven.members.create!(name: "Bonnie Nettles", married: true, children: 0)
     visit '/cults'
 
     save_and_open_page
-    expect(heaven).to appear_before(waco)
-    expect(waco).to appear_before(science)
+    expect(science.name).to appear_before(waco.name)
+    expect(waco.name).to appear_before(heaven.name)
   end
 end
