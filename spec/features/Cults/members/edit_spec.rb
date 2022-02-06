b45a9610eedd4aaba1ed572c89d837fe34fcd088
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-06 15:22:06 -0700

    test: US14 tests added

---
+++ spec/features/Cults/members/edit_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Edit cultist page" do
+  before :each do
+    @heaven = Cult.create!(name: "Heaven's Gate", member_size: 323, open_enrollment: true)
+    @waco = Cult.create!(name: "Branch Davidian", member_size: 42, open_enrollment: false)
+    @marshall = @heaven.members.create!(name: "Marshall Applewhite", married: true, children: 2)
+    @bonnie = @heaven.members.create!(name: "Bonnie Nettles", married: true, children: 0)
+    @david = @waco.members.create!(name: "David Khouresh", married: true, children: 32)
+  end
+
+  it "US 14, has a button to go to edit page" do
+    visit "/members/#{@marshall.id}"
+    click_link "Update Cultist Here"
+
+    expect(current_path).to eq("/members/#{@marshall.id}/edit")
+  end
+
+  it "will display a form to collect new data" do
+    visit "/members/#{@marshall.id}/edit"
+    expect(page).to have_content("Edit member name")
+    expect(page).to have_content('Children count:')
+    expect(page).to have_content('Marital status:')
+  end
+  
+
+end
