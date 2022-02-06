d35a569b616afb6a709deca658cec39f1e7abf9a
Ian Ross <ianrossdrums@gmail.com>
2022-02-06 10:15:16 -0700

    feat: user story 3 and 4 tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   #Joes work
   get '/members', to: 'members#index'
-  get '/members/:id', to: "members#show"
+  get '/members/:id', to: 'members#show'
   get '/cults', to: 'cults#index'
   get '/cults/:cult_id/members', to: 'cult_members#index'
   #get '/cults/members', to:
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   #Ians work
   get '/cages', to: 'cages#index'
   get '/cages/:id', to: 'cages#show'
+  get '/ferrets', to: 'ferrets#index'
+  get '/ferrets/:id', to: 'ferrets#show'
 end
