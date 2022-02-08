caf2cc63ad8c03d8fe8a3febfd950efa8046813f
Ian Ross <ianrossdrums@gmail.com>
2022-02-08 14:04:26 -0700

    feat: user story 6 tests pass

--- spec/features/cages/index_spec.rb
+++ spec/features/cages/index_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe 'cage index page' do
 
     expect(page).to have_content(cage.title)
   end
+  describe 'User Story 6' do
+    it 'order cages by creation time' do
+      cage = Cage.create!(title: "Stinky Land", is_open: true, ferret_count: 3, ferret_capacity: 5)
+      cage_2 = Cage.create!(title: "Pabu Kingdom", is_open: false, ferret_count: 4, ferret_capacity: 5)
+      cage_3 = Cage.create!(title: "Adorable", is_open: false, ferret_count: 1, ferret_capacity: 4)
+      visit "/cages"
+
+      expect(cage_3.title).to appear_before(cage.title)
+    end
+    it 'shows creation time' do
+      cage = Cage.create!(title: "Stinky Land", is_open: true, ferret_count: 3, ferret_capacity: 5)
+      cage_2 = Cage.create!(title: "Pabu Kingdom", is_open: false, ferret_count: 4, ferret_capacity: 5)
+      cage_3 = Cage.create!(title: "Adorable", is_open: false, ferret_count: 1, ferret_capacity: 4)
+      visit "/cages"
+
+      expect(page).to have_content(cage.created_at.strftime("%m/%d at %H:%M %p"))
+    end
+  end
 end
