1750895f28dbd9c7c094b34addffef473af85d50
Ian Ross <ianrossdrums@gmail.com>
2022-02-09 12:25:37 -0700

    user stories 8-10 pass

--- spec/features/cages/ferrets/index_spec.rb
+++ spec/features/cages/ferrets/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Ferret cage index page' do
-  describe 'User story ' do
+  describe 'User story 5' do
     it 'displays cage title' do
       cage = Cage.create!(title: "Stinky Land", is_open: true, ferret_count: 3, ferret_capacity: 5)
       ferret = cage.ferrets.create!(name: "Pabu", is_clean: false, age: 1)
@@ -21,6 +21,26 @@ RSpec.describe 'Ferret cage index page' do
       expect(page).to have_content(ferret_2.name)
       expect(page).to have_content(ferret.age)
       expect(page).to have_content(ferret_2.age)
+    end
+  end
+
+  describe 'User stories 8-10' do
+    before :each do
+      @cage = Cage.create!(title: "Stinky Kingdom", is_open: true, ferret_count: 3, ferret_capacity: 5)
+      @ferret = @cage.ferrets.create!(name: "Pabu", is_clean: false, age: 1)
+      @ferret_2 = @cage.ferrets.create!(name: "Loki", is_clean: true, age: 1)
+    end
+    it 'has child index link' do
+      visit "/cages"
+      expect(page).to have_selector(:link_or_button, "Ferret Index")
+    end
+    it 'has parent index link' do
+      visit "/cages"
+      expect(page).to have_selector(:link_or_button, "Cage Index")
+    end
+    it 'has link to parents child table page' do
+      visit "/cages/#{@cage.id}"
+      expect(page).to have_selector(:link_or_button, "#{@cage.title}s Ferrets")
     end
   end
 end
